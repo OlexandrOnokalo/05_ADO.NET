eadef776b3e0c6c9da76ce7f46f207b5cbdee755
OlexandrOnokalo <Olexandr.onokalo@gmail.com>
2025-09-08 17:45:38 +0300

    010_LAB

--- 01_ADO.NET_Intro_Connected_Mode/Program.cs
+++ 01_ADO.NET_Intro_Connected_Mode/Program.cs
@@ -1,10 +1,124 @@
-﻿namespace _01_ADO.NET_Intro_Connected_Mode
+﻿using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace _01_ADO.NET_Intro_Connected_Mode
 {
     internal class Program
     {
+        static void ShowAllClients(SqlConnection connection)
+        {
+            string cmdText = $@"select * from Clients;";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //// відображається назви всіх колонок таблиці
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                Console.Write($" {reader.GetName(i),14}");
+            }
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------------------------------");
+
+            //////// відображаємо всі значення кожного рядка
+            while (reader.Read())
+            {
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($" {reader[i],14} ");
+                }
+                Console.WriteLine();
+            }
+
+            reader.Close();
+
+        }
+
+        static void ShowAllEmployees(SqlConnection connection)
+        {
+            string cmdText = $@"select * from Employees;";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //// відображається назви всіх колонок таблиці
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                Console.Write($" {reader.GetName(i),14}");
+            }
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------------------------------");
+
+            //////// відображаємо всі значення кожного рядка
+            while (reader.Read())
+            {
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($" {reader[i],14} ");
+                }
+                Console.WriteLine();
+            }
+
+            reader.Close();
+
+        }
+
+
+        static void ShowSalesByEmployee(SqlConnection connection)
+        {
+            string cmdText = $@"select * from Employees;";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //// відображається назви всіх колонок таблиці
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                Console.Write($" {reader.GetName(i),14}");
+            }
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------------------------------");
+
+            //////// відображаємо всі значення кожного рядка
+            while (reader.Read())
+            {
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($" {reader[i],14} ");
+                }
+                Console.WriteLine();
+            }
+
+            reader.Close();
+
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string connectionString = @"Data Source = (localDB)\MSSQLLocalDb; 
+                                        Initial Catalog = SportShop;
+                                        Integrated Security = true; TrustServerCertificate=True;
+";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            Console.WriteLine("Connected success!");
+            ShowAllClients(sqlConnection);
+            ShowAllEmployees(sqlConnection);
+
+
+
+
+
+
+            sqlConnection.Close();
         }
     }
 }
